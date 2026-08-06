1c3497462e90e259b1300a41720737b5d0bc19ea
Hugo <chagas.hugo@gmail.com>
2026-08-05 21:36:37 -0300

    perf: abiFilters arm64/arm32 (v1.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,11 +18,12 @@ android {
         applicationId = "br.com.lit.busca.uc"
         minSdk        = 24
         targetSdk     = 36
-        versionCode   = 2
-        versionName   = "1.1"
+        versionCode   = 3
+        versionName   = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
+        ndk { abiFilters += listOf("arm64-v8a", "armeabi-v7a") }
     }
 
     signingConfigs {
